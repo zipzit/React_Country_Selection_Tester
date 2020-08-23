610da0bc26f5143aaf9c994eab3e36533a5f87aa
Zip <zipzit@yahoo.com>
2020-08-23 06:57:54 -0700

    Update CountryCard.js
    Add comma output to population #'s

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./CountryCard.css";
 
+function numberWithCommas(x) {  // S.O.2901102
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 const CountryCard = ({ country }) => {
+  let population = numberWithCommas(country.population);
+  
   return (
     <div className="country-card">
       <h3>{country.name}</h3>
@@ -13,7 +19,7 @@ const CountryCard = ({ country }) => {
         </li>
         <li className="country-info-item">
           <h4>Population:</h4>
-          <p>{country.population}</p>
+          <p>{population}</p>
         </li>
         <li className="country-info-item">
           <h4>Region:</h4>
